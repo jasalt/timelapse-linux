57c8a25d2da9b9638d303022f774de88dc4b45ca
Jarkko Saltiola <jarkko.saltiola@koodilehto.fi>
2015-11-21 09:24:38 +0200

    Fix darktime check.

--- scripts/capture/its_not_dark.py
+++ scripts/capture/its_not_dark.py
@@ -4,7 +4,8 @@
 # Script that tells if there's light outside (in Jyväskylä / Finland) at this
 # moment. Depends on ephem library, written for use with Python 2.7.
 import ephem
-from datetime import timedelta
+from datetime import timedelta, datetime
+from time import time
 
 o = ephem.Observer()
 o.lat = '62'  # <-- Customize latitude & longtitude to your liking
@@ -16,13 +17,21 @@ treshold = timedelta(hours=2)
 
 last_setting = ephem.localtime(o.previous_setting(s))
 last_dark = last_setting + treshold
+time_dark = last_dark.time()
 
+# ei asetu mikäli ei olla jo uudessa päivässä
 last_rising = ephem.localtime(o.previous_rising(s))
 last_light = last_rising - treshold
+time_light = last_light.time()
 
-if last_light < last_dark:
-    print("It's dark!")
-    exit(1)
-else:
+#dtn = datetime(2015, 10, 11, 4, 0)
+dtn = datetime.now()
+time_now = dtn.time()
+
+
+if time_light < time_now < time_dark:
     print("There's light!")
     exit(0)
+else:
+    print("It's dark!")
+    exit(1)
